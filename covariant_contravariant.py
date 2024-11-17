7860a1cad71f94daa6de9f2f4b8108abc7ff1123
norikenpi <norikenpi@gmail.com>
2024-11-18 06:00:35 +0900

    add OK NG pattern

--- covariant_contravariant.py
+++ covariant_contravariant.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
+from typing import TypeVar, Generic
+from abc import ABC, abstractmethod
 
+Input = TypeVar("Input", contravariant=True)
+Output = TypeVar("Output", covariant=True)
+
+class Animal: pass
+class Dog(Animal): pass
+
+class AnimalProcessor(Generic[Input, Output], ABC):
+	@abstractmethod
+	def process(self, input: Input) -> Output:
+		pass
+
+class DogtoAnimalProcessor(AnimalProcessor[Dog, Animal]):
+	def process(self, input: Dog) -> Animal:
+		return input # DogをそのスーパータイプであるAnimalとして返すのはLSPに則っている
+
+class AnimaltoDogProcessor(AnimalProcessor[Animal, Dog]):
+	def process(self, input: Animal) -> Dog:
+		dog = Dog()
+		return dog 
 
 if __name__ == "__main__":
-	print("Hello World")
+	processor1: AnimalProcessor[Dog, Animal] = DogtoAnimalProcessor() # OK
+	processor2: AnimalProcessor[Animal, Dog] = AnimaltoDogProcessor() # OK
+	processor3: AnimalProcessor[Dog, Animal] = AnimaltoDogProcessor() # OK
+	processor4: AnimalProcessor[Animal, Dog] = DogtoAnimalProcessor() # NG
